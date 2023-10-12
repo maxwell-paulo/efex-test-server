6d6dc4ef316c04dab0896313d613c32596355250
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-12 15:38:38 -0300

    feat: create login route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/usercontrollers.js";
+import { registerUser, login } from "../controllers/usercontrollers.js";
 import { validateRegistration } from "../middlewares/validateRegistration.js";
+import { validateLoginData } from "../middlewares/authMiddleware.js";
 
 export const router = Router();
 
 router.post("/user", validateRegistration, registerUser);
+
+router.post("/login", validateLoginData, login);
