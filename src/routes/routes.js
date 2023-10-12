0455075107adcf3e0a688a863f8befe38881e260
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-12 14:09:14 -0300

    feat: create user route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,2 +1,7 @@
 import { Router } from "express";
+import { registerUser } from "../controllers/usercontrollers.js";
+import { validateRegistration } from "../middlewares/validateRegistration.js";
+
 export const router = Router();
+
+router.post("/user", validateRegistration, registerUser);
