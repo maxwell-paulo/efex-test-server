9ba3d4b2a6e399a01129ce04389e584b83774b43
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-12 16:41:54 -0300

    refactor: refactor create task repository

--- src/repositories/taskRepository.js
+++ src/repositories/taskRepository.js
@@ -1,14 +1,14 @@
 import pool from "../database/database.js";
 
 export const createdTask = async (taskData) => {
-  const { name, description, user_id } = taskData;
+  const { name, description, user_id, done } = taskData;
 
   const query = `INSERT INTO tasks
-      (name, description, user_id)
-      VALUES ($1, $2, $3)
+      (name, description, user_id, done)
+      VALUES ($1, $2, $3, $4)
       returning *`;
 
-  const params = [name, description, user_id];
+  const params = [name, description, user_id, done];
   const newUser = await pool.query(query, params);
 
   return newUser.rows[0];
