a96e5ef33189008eadd1d60ff31785453ec77956
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-12 16:34:21 -0300

    feat: create task route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import { registerUser, login } from "../controllers/usercontrollers.js";
 import { validateRegistration } from "../middlewares/validateRegistration.js";
-import { validateLoginData } from "../middlewares/authMiddleware.js";
+import {
+  validateLoginData,
+  validateToken,
+} from "../middlewares/authMiddleware.js";
+
+import { createTask } from "../controllers/taskcontrollers.js";
 
 export const router = Router();
 
 router.post("/user", validateRegistration, registerUser);
 
 router.post("/login", validateLoginData, login);
+
+router.use(validateToken);
+
+router.post("/task", createTask);
