141f948334ff0a3aba656cddf610a460f44cede3
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-12 14:07:29 -0300

    refactor: change .env.example and dump.sql

--- dump.sql
+++ dump.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   email VARCHAR(100) NOT NULL UNIQUE,
-  senha TEXT NOT NULL
+  password TEXT NOT NULL
 )
 
 CREATE TABLE tasks (
