306ce205b885ecfc6a72f359f88b01d6c8bc2f74
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-12 15:38:00 -0300

    feat: create validade login data middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,3 +2,16 @@ import {
   checkEncrypt,
   validateAndGetTokenData,
 } from "../utils/encryptPassword.js";
+import { findUserByEmail } from "../repositories/userRepository.js";
+
+export const validateLoginData = async (req, res, next) => {
+  const { email, password } = req.body;
+  const userData = await findUserByEmail(email);
+  const isValidEncrypt = await checkEncrypt(userData?.password, password);
+
+  if (!isValidEncrypt) {
+    return res.status(401).json({ message: "Acesso não autorizado." });
+  }
+
+  next();
+};
