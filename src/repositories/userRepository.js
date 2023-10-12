1c727bdbac048467d12267b69adc1b5ab6dbe20a
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-12 14:08:49 -0300

    feat: creat user repository

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,2 +1,16 @@
 import pool from "../database/database.js";
 import { encryptPassword } from "../utils/encryptPassword.js";
+
+export const createdUser = async (userData) => {
+  const { name, email, password } = userData;
+
+  const query = `INSERT INTO users
+    (name, email, password)
+    VALUES ($1, $2, $3)
+    returning *`;
+
+  const params = [name, email, await encryptPassword(password)];
+  const newUser = await pool.query(query, params);
+
+  return newUser.rows[0];
+};
