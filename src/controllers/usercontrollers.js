df09f461bf24f3de7dc8fb4ca22bf501966821e9
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-12 15:37:23 -0300

    feat: creat login controller

--- src/controllers/usercontrollers.js
+++ src/controllers/usercontrollers.js
@@ -1,5 +1,6 @@
-import handleServerError from "../utils/serverError.js";
 import { createdUser } from "../repositories/userRepository.js";
+import { findUserByEmail } from "../repositories/userRepository.js";
+import { generateToken } from "../utils/encryptPassword.js";
 
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
@@ -13,4 +14,36 @@ const registerUser = async (req, res) => {
   }
 };
 
-export { registerUser };
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and/or password not provided." });
+    }
+
+    const user = await findUserByEmail(email);
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ mensagem: "E-mail e/ou senha inválido(s)." });
+    }
+
+    const { id } = user;
+    const optionsPayload = { expiresIn: "8h" };
+    const token = generateToken({ id }, optionsPayload);
+    delete user.password;
+
+    return res.status(200).json({ user, token });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      mensagem: error.message,
+    });
+  }
+};
+
+export { registerUser, login };
